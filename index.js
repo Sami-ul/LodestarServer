5226b20eb1bb2770aee790eb3f02ef5c6e237c5c
Sami <72280649+Sami-ul@users.noreply.github.com>
2022-06-25 08:21:02 +0000

    rm cors

--- index.js
+++ index.js
@@ -3,23 +3,15 @@ const express = require('express'); // Used for incoming API requests
 const bodyParser = require('body-parser'); // middleware for express
 const online = require('./routes/online.js');
 const fs = require('fs');
-var cors = require('cors');
 
 // Sets up express
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 pathWalk('./routes/');
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'});
-});
 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80');
-});
 async function pathWalk(dir) {
   // Walks the routes folder and turns on all endpoints, ignoring files marked func-
   const directory = await fs.promises.readdir(dir);
